438f4594135e66e2474b345637ca8d599f0b6ab4
Saraphoo <sarah.riley@sourcetoad.com>
2025-03-19 09:15:03 -0400

    Building Pet Cards

--- resources/js/pages/dashboard/PetProfileCard.vue
+++ resources/js/pages/dashboard/PetProfileCard.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 
 import {onMounted} from "vue";
+import {Card} from "@/components/ui/card";
 
 defineProps<{
     pet: {
         id: number;
         name: string;
-        petImage: string;
+        petImage: string | null;
         type: string;
         DOB: string;
     };
@@ -20,9 +21,13 @@ onMounted(() => {
 </script>
 
 <template>
-<Card>
+<Card class ="items-center">
     <div class="relative mt-8 flex items-center gap-x-4">
-
+        <div v-if="pet.petImage">
+            <img :src="pet.petImage" alt="pet image" class="w-16 h-16 rounded-full" />
+        </div>
+            <div v-else class="w-16 h-16 rounded-full bg-gray-200">
+            </div>
         <div class="text-sm/6">
             <p class="font-semibold text-gray-900">
                 <a href="#">
@@ -30,7 +35,7 @@ onMounted(() => {
                     {{pet.name}}
                 </a>
             </p>
-            <p class="text-gray-600">{{pet.type}} | {{pet.age}}</p>
+            <p class="text-gray-600">{{pet.type}} | {{pet.DOB}}</p>
         </div>
     </div>
 </Card>
